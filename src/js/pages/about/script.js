eddf920a34cd95ff78fdaa925061dbe02cd04b20
Alrenado <eugene1998@ukr.net>
2025-08-21 20:48:28 +0300

    fix: path

--- src/js/pages/about/script.js
+++ src/js/pages/about/script.js
@@ -1,5 +1,10 @@
 import select from "../../components/assets/select.js";
+import dialog from "../../components/dialog.js";
 
 document.addEventListener("DOMContentLoaded", () => {
     select();
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    dialog();
 });
