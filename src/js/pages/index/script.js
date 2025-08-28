c2530ff1917194529adab52388ddf19a96065e50
Alrenado <eugene1998@ukr.net>
2025-08-28 18:08:51 +0300

    feat: picture observer

--- src/js/pages/index/script.js
+++ src/js/pages/index/script.js
@@ -1,10 +1,12 @@
 import select from "../../components/assets/select.js";
 import input from "../../components/assets/input.js";
+import picture from "../../components/assets/picture.js";
 import header from "../../components/header.js";
 import dialog from "../../components/dialog.js";
 
 
 document.addEventListener("DOMContentLoaded", () => {
+    picture();
     input();
     header();
     dialog();
